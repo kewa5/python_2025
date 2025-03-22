8abe5c867a3192a5256afd68107d91ee4889fb69
kewa5 <wojtysiakewa5@gmail.com>
2025-03-22 12:07:38 +0100

    sortowanie

---
+++ sortowanie/babelkowe_funkcja.py
@@ -0,0 +1,9 @@
+def sortuj_przez_babelkowe (dane):
+    n = len (dane)
+        for i in range (0, n-1):
+        for j in range(0, n-1-i):
+            if dane[j] > dane [j+1]:
+                 dane[j],dane[j+1] = dane[j + 1],dane[j]
+
+
+
--- sortowanie/sortowanie.py
+++ sortowanie/sortowanie.py
@@ -1,1 +1,6 @@
-import
+import babelkowe_funkcja
+if __name__ == '__main__':
+
+    dane = [2, 7, 5, 8, 1]
+    babelkowe_funkcja.sortuj_przez_babelkowe(dane)
+    print(dane)
