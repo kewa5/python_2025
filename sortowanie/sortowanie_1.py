352551d7aa7e60f48017db04cf5cb0ebea4ca315
kewa5 <wojtysiakewa5@gmail.com>
2025-03-22 12:25:42 +0100

    sortowanie

---
+++ sortowanie/sortowanie_1.py
@@ -0,0 +1,6 @@
+import wybieranie
+if __name__ == '__main__':
+
+    dane = [2, 7, 5, 8, 1]
+    wybieranie.sortuj_wybieranie(dane)
+    print(dane)
---
+++ sortowanie/sortowanie_2.py
@@ -0,0 +1,6 @@
+import wstawianie
+if __name__ == '__main__':
+
+    dane = [2, 7, 5, 8, 5, 4, 1]
+    wstawianie.sortuj_wstawianie(dane)
+    print(dane)
