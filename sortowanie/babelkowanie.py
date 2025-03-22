de50bd691382b1fa65c963fc768885863826ae68
kewa5 <wojtysiakewa5@gmail.com>
2025-03-22 11:53:27 +0100

    sortowanie

--- sortowanie/babelkowanie.py
+++ sortowanie/babelkowanie.py
@@ -1,11 +1,14 @@
 dane = [2, 5, 7, 9, 1, 4, 2]
 n = len (dane)
-licznik = 0
+licznik_z = 0
+licznik_p = 0
 for i in range (0, n-1):
     for j in range(0, n-1-i):
         if dane[j] > dane [j+1]:
              dane[j],dane[j+1] = dane[j + 1],dane[j]
-             licznik = licznik + 1
+             licznik_z = licznik_z + 1
+        licznik_p = licznik_p + 1
     print(dane)
 print (dane)
-print (licznik)
+print (licznik_z)
+print (licznik_p)
